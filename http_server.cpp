b65fb43210c1fdd82fd5bf6d6426d806612b3652
ubuntu <hjkja0511@gmail.com>
2024-05-02 12:31:47 +0000

    feat: add basic feature

--- http_server.cpp
+++ http_server.cpp
@@ -1,8 +1,114 @@
-#include<iostream>
+#include <boost/asio.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <utility>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+using boost::asio::ip::tcp;
 using namespace std;
 
-int main(){
-    
+struct Request {
+    const string env_Variables[9] = {
+        "REQUEST_METHOD",  "REQUEST_URI", "QUERY_STRING",
+        "SERVER_PROTOCOL", "HTTP_HOST",   "SERVER_ADDR",
+        "SERVER_PORT",     "REMOTE_ADDR", "REMOTE_PORT"};
+    string value[9];
+};
+
+const string env_Variables[9] = {
+    "REQUEST_METHOD",  "REQUEST_URI", "QUERY_STRING",
+    "SERVER_PROTOCOL", "HTTP_HOST",   "SERVER_ADDR",
+    "SERVER_PORT",     "REMOTE_ADDR", "REMOTE_PORT"};
+
+
+class session : public std::enable_shared_from_this<session> {
+  public:
+    session(tcp::socket socket) : socket_(std::move(socket)) {}
+
+    void start() { do_read(); }
+
+  private:
+    void http_request_parser() {
+
+    }
+
+    void do_read() {
+        auto self(shared_from_this());
+
+        socket_.async_read_some(
+            boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+
+                    data_[length] = '\0';
+                    cout << data_ << endl;
+                    http_request_parser();
+                }
+            });
+    }
+
+    void do_write(std::size_t length) {
+        auto self(shared_from_this());
+        boost::asio::async_write(
+            socket_, boost::asio::buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+                if (!ec) {
+                    do_read();
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+    pair<string, string> env[9];
+};
+
+class server {
+  public:
+    server(boost::asio::io_context &io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        do_accept();
+    }
+
+  private:
+    void do_accept() {
+        acceptor_.async_accept(
+            [this](boost::system::error_code ec, tcp::socket socket) {
+                if (!ec) {
+                    std::make_shared<session>(std::move(socket))->start();
+                }
+
+                do_accept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+};
+
+int main(int argc, char *argv[]) {
+    std::cout << BOOST_LIB_VERSION << '\n';
+    return 0;
+    try {
+        if (argc != 2) {
+            std::cerr << "Usage: async_tcp_echo_server <port>\n";
+            return 1;
+        }
+
+        boost::asio::io_context io_context;
+
+        server s(io_context, std::atoi(argv[1]));
+
+        io_context.run();
+    } catch (std::exception &e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
 
     return 0;
 }
